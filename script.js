05ee9b4547764cb2d2e34f6f38668cd5975abb1b
lasha jijelava <lasha0990@gmail.com>
2024-08-06 21:12:23 +0400

    Add jsfunctions_6

---
+++ script.js
@@ -0,0 +1,43 @@
+// 1.
+
+// function toggleModal() {
+//     const modalContainer = document.getElementById('modalContainer');
+//     modalContainer.classList.toggle('hidden');
+// }
+
+
+// 2.
+
+// const validColors = ['red', 'blue', 'green', 'black', 'white'];
+
+//         const changeColorButton = document.getElementById('changeColorButton');
+
+//         changeColorButton.addEventListener('click', function() {
+//             const colorInput = document.getElementById('colorInput').value.toLowerCase();
+
+//             if (validColors.includes(colorInput)) {
+//                 document.body.style.backgroundColor = colorInput;
+//             } else {
+//                 alert('Invalid color! Please enter one of the following: red, blue, green, black, white.');
+//             }
+//         });
+
+
+// 3.
+
+// const averageButton = document.getElementById('averageButton');
+//     const resultDiv = document.getElementById('result');
+
+//     averageButton.addEventListener('click', function() {
+//         const input = document.getElementById('numbersInput').value;
+
+//         const numbers = input.split(':').map(Number);
+
+//         const sum = numbers.reduce((acc, num) => acc + num, 0);
+//         const average = sum / numbers.length;
+
+//         resultDiv.textContent = `Average: ${average}`;
+// });
+
+
+console.log('Everything is done')
